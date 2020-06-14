a8d4d4591f0696c664d95addc749dd1cc5a9f643
Deepak Bhasakran <deepak.ny007@gmail.com>
2020-06-14 12:15:06 -0700

    Added CORS filter for Stats

--- src/main/java/org/dbhaskaran/covid19/controllers/CovidRestController.java
+++ src/main/java/org/dbhaskaran/covid19/controllers/CovidRestController.java
@@ -6,6 +6,7 @@ import org.dbhaskaran.covid19.entities.Covid;
 import org.dbhaskaran.covid19.entities.Stats;
 import org.dbhaskaran.covid19.service.CovidServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,16 +18,19 @@ public class CovidRestController {
 	@Autowired
 	CovidServiceImpl covidService;
 
+	@CrossOrigin
 	@GetMapping(value = "countries/", produces = "application/json")
 	public List<Covid> getAll() {
 		return covidService.getAllCovid();
 	}
 
+	@CrossOrigin
 	@GetMapping(value = "countries/{country}", produces = "application/json")
 	public List<Covid> getByCountry(@PathVariable String country) {
 		return covidService.searchCovid(country);
 	}
 	
+	@CrossOrigin
 	@GetMapping(value = "stats/", produces = "application/json")
 	public Stats getStats() {
 		return covidService.getStats();
